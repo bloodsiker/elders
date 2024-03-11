6e1949b19a024820d505562c3cf9be9120ebce62
Dmytro Ovsiichuk <dmytro.ovsiichuk@stfalcon.com>
2024-03-11 07:09:37 +0200

    Fix table

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -19,7 +19,7 @@
                             @foreach($location->items as $item)
                                 <tr style="vertical-align: middle;">
                                     <td style="width: 67px"><img src="{{ asset($item->image) }}" alt=""></td>
-                                    <td style="padding: 19px 10px; font-size: 16px"><span class="item_name">{{ $item->name }}</span></td>
+                                    <td style="padding: 19px 10px; font-size: 16px"><a href="{{ route('item.details', ['id' => $item->id]) }}" class="line">{{ $item->name }}</a></td>
                                     <td style="width: 50px;padding: 19px;font-size: 16px;">{{ $item->pivot->quantity }}</td>
                                 </tr>
                             @endforeach
