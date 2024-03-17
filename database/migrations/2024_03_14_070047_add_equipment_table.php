a7f869304b679b66805c6af5c61152bb4e2e74d2
Dmytro Ovsiichuk <dmytro.ovsiichuk@stfalcon.com>
2024-03-17 18:13:09 +0200

    Added relevation items

--- database/migrations/2024_03_14_070047_add_equipment_table.php
+++ database/migrations/2024_03_14_070047_add_equipment_table.php
@@ -17,6 +17,7 @@ class AddEquipmentTable extends Migration
             $table->id();
             $table->string('type', 30)->nullable();
             $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
 
